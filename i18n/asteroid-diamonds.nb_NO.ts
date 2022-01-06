176cfa36c4dd2fa44614944107cd1502f0fc360c
Allan Nordhøy <epost@anotheragency.no>
2022-01-06 18:12:59 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.nb_NO.ts
+++ i18n/asteroid-diamonds.nb_NO.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>Poengsum</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>Best</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Spillet er over</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Prøv igjen</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamanter</translation>
     </message>
 </context>
 </TS>
