4a2c13b304728332929c660cbd31c0b9e333e129
Yaron Shahrabani <sh.yaron@gmail.com>
2022-01-07 14:55:57 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.he.ts
+++ i18n/asteroid-diamonds.he.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>ניקוד</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>הטוב ביותר</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>המשחק נגמר</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>לנסות שוב</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>יהלומים</translation>
     </message>
 </context>
 </TS>
