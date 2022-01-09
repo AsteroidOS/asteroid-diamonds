d0e4237c3871344c727fdc14b162de3dd9620e80
Luna Jernberg <droidbittin@gmail.com>
2022-01-09 12:41:08 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.sv.ts
+++ i18n/asteroid-diamonds.sv.ts
@@ -11,7 +11,7 @@
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished">Best</translation>
+        <translation>BEST</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
