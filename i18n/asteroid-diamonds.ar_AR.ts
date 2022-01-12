2ab869f1b2a7b2911aea026afbc3c5bfeb552566
Rex_sa <rex.sa@pm.me>
2022-01-12 05:33:46 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.ar_AR.ts
+++ i18n/asteroid-diamonds.ar_AR.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>النتيجة</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>الأفضل</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>انتهت اللعبة</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>حاول مرة أخرى</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>الألماس</translation>
     </message>
 </context>
 </TS>
