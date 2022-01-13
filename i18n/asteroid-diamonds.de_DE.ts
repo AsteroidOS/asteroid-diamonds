1391ae70a1767e9a040b9d6c8137469ee94507ba
Timo Könnecke <koennecke@mosushi.de>
2022-01-13 20:01:58 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.de_DE.ts
+++ i18n/asteroid-diamonds.de_DE.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>PUNKTE</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>BESTE</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Spiel Vorbei</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Versuchs Nochmal</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamanten</translation>
     </message>
 </context>
 </TS>
