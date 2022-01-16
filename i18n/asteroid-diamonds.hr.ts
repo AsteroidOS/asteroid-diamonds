7bf7f5e5177a13b36ba0e05d62829b8c621574f3
Milo Ivir <mail@milotype.de>
2022-01-16 20:59:39 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.hr.ts
+++ i18n/asteroid-diamonds.hr.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>REZULTAT</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>NAJBOLJI</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Kraj igre</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokušaj ponovo</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Dijamanti</translation>
     </message>
 </context>
 </TS>
