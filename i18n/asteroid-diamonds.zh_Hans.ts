160f69fe1801feb9955df43aff4ba3022996fe6b
poi <erbaotao@outlook.com>
2022-01-24 15:33:09 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 80.0% (4 of 5 strings)

--- i18n/asteroid-diamonds.zh_Hans.ts
+++ i18n/asteroid-diamonds.zh_Hans.ts
@@ -6,22 +6,22 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>得分</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>第一</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>游戏结束</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>再来一次</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
