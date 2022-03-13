2b045384b18366721f5d9ebd04f40074947d07f9
Tsuki <danghoangtuan526@gmail.com>
2022-03-13 05:50:37 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.vi.ts
+++ i18n/asteroid-diamonds.vi.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>Điểm số</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>Tốt nhất</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Trò chơi kết thúc</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Thử lại</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Kim cương</translation>
     </message>
 </context>
 </TS>
