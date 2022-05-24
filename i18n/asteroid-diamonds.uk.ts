3cbd96559080cee7f4f9cbb9273f02b619356c84
Artem <artem@molotov.work>
2022-05-24 17:28:21 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.uk.ts
+++ i18n/asteroid-diamonds.uk.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>РАХУНОК</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>КРАЩИЙ</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Гра завершена</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Наново</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Діаманти</translation>
     </message>
 </context>
 </TS>
