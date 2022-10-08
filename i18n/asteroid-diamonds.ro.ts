615300379ed2d6ce30286617bb021acf684280fd
Florin Voicu <florin.bkk@gmail.com>
2022-10-08 21:35:32 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.ro.ts
+++ i18n/asteroid-diamonds.ro.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>SCOR</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>CEL MAI BUN</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Joc Terminat</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Încearcă din nou</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamante</translation>
     </message>
 </context>
 </TS>
