e092131c1b4ff48594b14e330e98351a9a582630
Enol P <enolp@softastur.org>
2022-10-17 23:06:27 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.ast.ts
+++ i18n/asteroid-diamonds.ast.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>PUNTUACIÓN</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>LO MEYOR</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Acabóse la partida</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Retentar</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamantes</translation>
     </message>
 </context>
 </TS>
