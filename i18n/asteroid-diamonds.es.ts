6dc51bb87af9b9818132e9051dd4b824800ba2b8
gallegonovato <fran-carro@hotmail.es>
2022-11-09 20:09:58 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.es.ts
+++ i18n/asteroid-diamonds.es.ts
@@ -6,7 +6,7 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>PUNTUACIÓN</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
