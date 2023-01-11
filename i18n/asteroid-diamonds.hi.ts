293f6e536100b84dfdb7569be5443895ac786054
Xyngg <heyabhinav@protonmail.com>
2023-01-11 20:36:59 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.hi.ts
+++ i18n/asteroid-diamonds.hi.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>अंक</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>श्रेष्ठ</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>खेल खत्म</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>पुनः प्रयास करें</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>हीरे</translation>
     </message>
 </context>
 </TS>
