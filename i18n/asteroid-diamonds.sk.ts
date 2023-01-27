bea2da751634f2c75b9921eadfc166b0015ae75b
aasami <aasami@gmail.com>
2023-01-27 10:55:13 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.sk.ts
+++ i18n/asteroid-diamonds.sk.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>SKÓRE</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>NAJ</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Hra skončila</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Skúsiť znova</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamanty</translation>
     </message>
 </context>
 </TS>
