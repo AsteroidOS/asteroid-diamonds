424fcf1a4c1c823c779fee9ed1d34bc0c88c7e98
Software In Interlingua <softinterlingua@gmail.com>
2024-01-25 23:49:53 +0000

    Translated using Weblate (Interlingua)
    Currently translated at 60.0% (3 of 5 strings)

--- i18n/asteroid-diamonds.ia.ts
+++ i18n/asteroid-diamonds.ia.ts
@@ -11,7 +11,7 @@
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>MELIOR</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
@@ -21,12 +21,12 @@
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Retenta lo</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamantes</translation>
     </message>
 </context>
 </TS>
