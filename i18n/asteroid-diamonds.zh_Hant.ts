3d7951e0823afecb13107d53293242a14e3ee179
hugoalh <hugoalh@users.noreply.hosted.weblate.org>
2024-05-14 09:43:57 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.zh_Hant.ts
+++ i18n/asteroid-diamonds.zh_Hant.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>分數</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>最佳</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>遊戲結束</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>再試一次</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>鑽石</translation>
     </message>
 </context>
 </TS>
