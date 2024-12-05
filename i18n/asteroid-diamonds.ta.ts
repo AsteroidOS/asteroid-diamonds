5a73fd5d74a15819f12cbda9aab028d33b565476
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-12-05 15:10:59 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.ta.ts
+++ i18n/asteroid-diamonds.ta.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>கெலிப்பெண்</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>சிறந்த</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>விளையாட்டு ஓவர்</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>மீண்டும் முயற்சிக்கவும்</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>வைரங்கள்</translation>
     </message>
 </context>
 </TS>
