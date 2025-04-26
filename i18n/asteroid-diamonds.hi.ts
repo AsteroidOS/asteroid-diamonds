d3fab5183f4480535dd1abcdbeffd0cb6cf1196b
Govind Gopal Yadav <govindgopalgaming@gmail.com>
2025-04-26 19:30:55 +0200

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.hi.ts
+++ i18n/asteroid-diamonds.hi.ts
@@ -6,22 +6,22 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation>अंक</translation>
+        <translation>स्कोर</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation>श्रेष्ठ</translation>
+        <translation>सबसे अच्छा</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation>खेल खत्म</translation>
+        <translation>खेल समाप्त</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation>पुनः प्रयास करें</translation>
+        <translation>दोबारा कोशिश करें</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
