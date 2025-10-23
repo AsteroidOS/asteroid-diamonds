b43461d6ed087265db2ae4c797568e9bead80b92
Artur Ajvazjan <arturaiwasan@gmail.com>
2025-10-23 22:02:42 +0200

    Translated using Weblate (Interslavic)
    Currently translated at 100.0% (5 of 5 strings)

--- i18n/asteroid-diamonds.isv.ts
+++ i18n/asteroid-diamonds.isv.ts
@@ -6,27 +6,27 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>REZULTAT</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>Najlěpšy</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Igra dokončena</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Povtoriti</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
         <source>Diamonds</source>
-        <translation type="unfinished"></translation>
+        <translation>Diamanty</translation>
     </message>
 </context>
 </TS>
