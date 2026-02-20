2c587a088992ab3f04b3c643219c1735b2d1bb98
Quentin PAGÈS <quentinantonin@free.fr>
2026-02-20 10:03:35 +0100

    Translated using Weblate (Occitan)
    Currently translated at 80.0% (4 of 5 strings)

--- i18n/asteroid-diamonds.oc.ts
+++ i18n/asteroid-diamonds.oc.ts
@@ -6,22 +6,22 @@
     <message id="id-score">
         <location filename="../src/Game.qml" line="319"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>MARCA</translation>
     </message>
     <message id="id-best">
         <location filename="../src/Game.qml" line="352"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>MELHOR</translation>
     </message>
     <message id="id-game-over">
         <location filename="../src/Game.qml" line="572"/>
         <source>Game Over</source>
-        <translation type="unfinished"></translation>
+        <translation>Fin de partida</translation>
     </message>
     <message id="id-try-again">
         <location filename="../src/Game.qml" line="591"/>
         <source>Try Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Tornatz ensajar</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-diamonds.desktop.h" line="6"/>
